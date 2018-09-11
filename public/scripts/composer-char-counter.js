73c51b12783ab6c1e131e674fe8d69b379c4a892
Paula Barreto <paulavilaca@gmail.com>
2018-09-10 20:43:31 -0400

    counter working fine

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  var chars = 140;
+  $ (".counter").html(140);
+  $(".text").keyup(function() {
+    let value = $(this).val();
+    chars = 140 - value.length;
+    let counter = $(this).siblings(".counter");
+    counter.html(chars);
+    if(chars < 0){
+      $(this).siblings(".counter").css("color", "red");
+    }
+  });
+});
