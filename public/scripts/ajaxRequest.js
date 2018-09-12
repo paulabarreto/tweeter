6e21ac69b32cfefff8da4a6fecfcc82d7b79f542
Paula Barreto <paulavilaca@gmail.com>
2018-09-12 12:23:04 -0400

    validation

--- public/scripts/ajaxRequest.js
+++ public/scripts/ajaxRequest.js
@@ -2,6 +2,11 @@ $(document).ready(function(){
   $("#new-tweet").on("submit", function (event){
     event.preventDefault();
     let str = $(this).serialize();
-    console.log(str);
+    if(str.length > 140){
+      alert("Tweet exceeds maximum allowed");
+    }
+    if(str === "text="){
+      alert("Please write something to tweet!");
+    }
   })
 });
