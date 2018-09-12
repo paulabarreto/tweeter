42ffbd461701e7d47b77fea61bf79527c3de6dd3
Paula Barreto <paulavilaca@gmail.com>
2018-09-12 12:54:27 -0400

    tweets without refreshing page

--- public/scripts/ajaxRequest.js
+++ public/scripts/ajaxRequest.js
@@ -2,11 +2,15 @@ $(document).ready(function(){
   $("#new-tweet").on("submit", function (event){
     event.preventDefault();
     let str = $(this).serialize();
-    if(str.length > 140){
-      alert("Tweet exceeds maximum allowed");
-    }
-    if(str === "text="){
-      alert("Please write something to tweet!");
-    }
-  })
+    $.post("/tweets", str, function(){
+      if(str.length > 140){
+        alert("Tweet exceeds maximum allowed");
+      }
+      if(str === "text="){
+        alert("Please write something to tweet!");
+      } else{
+        location.reload();
+      }
+    });
+  });
 });
